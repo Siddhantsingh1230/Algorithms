ff2b29d43df9f54ac1de07b2ff3131819b8937d2
siddhantsingh1230 <siddhantsingh9023134084@gmail.com>
2023-12-09 19:32:14 +0530

    optimized bubble

--- Optimised BubbleSort.c
+++ Optimised BubbleSort.c
@@ -10,10 +10,10 @@ void printLoop(int arr[], int size)
 
 void bubbleSort(int *arr, int size)
 {
-    int flag;
     for (int i = 0; i < size - 1; i++)
     {
-        flag = 0;
+
+        int flag = 1;
         for (int j = 0; j < size - i - 1; j++)
         {
             if (arr[j] > arr[j + 1])
@@ -21,19 +21,18 @@ void bubbleSort(int *arr, int size)
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
-                flag = 1;
+                flag = 0;
             }
         }
-        if (!flag)
+        if (flag)
         {
             break;
         }
     }
 }
-
 int main()
 {
-    int arr[] = {5, 6,1, 1, 2, 7, 4};
+    int arr[] = {5, 6, 1, 1, 2, 7, 4};
     int size = sizeof(arr) / sizeof(arr[0]);
     bubbleSort(arr, size);
     printLoop(arr, size);
